f89922569f69228cf927b0b7e7ef2250da336dd9
Eric Bailey <eric@ericb.me>
2016-04-01 20:45:36 -0500

    Update src/elli_fileserve.erl
    - Import useful functions from filename for brevity
    - Inline short functions and case clauses
    - Align `->`s
    - `s/<<"">>/<<>>/g`
    - Ensure no line is wider than 80 characters
    - Fix a few indentation issues

--- src/elli_fileserve.erl
+++ src/elli_fileserve.erl
@@ -15,6 +15,8 @@
 %% exported for mockability in tests (called through ?MODULE)
 -export([file_size/1]).
 
+-import(filename, [dirname/1, extension/1, flatten/1, join/1]).
+
 -ifdef(TEST).
 -compile([export_all]).
 -endif.
@@ -22,38 +24,31 @@
 
 handle(Req, Config) ->
     case unprefix(elli_request:raw_path(Req), prefix(Config)) of
-        undefined ->
-            ignore;
-        FilePath ->
+        undefined -> ignore;
+        FilePath  ->
             Filename = local_path(Config, FilePath),
             case ?MODULE:file_size(Filename) of
-                {error, illegal_path} ->
-                    {403, [], <<"Not Allowed">>};
-                {error, _Reason} ->
-                    {404, [], <<"File Not Found">>};
+                {error, illegal_path} -> {403, [], <<"Not Allowed">>};
+                {error, _Reason}      -> {404, [], <<"File Not Found">>};
                 {ok, Size} ->
-                    {200, headers(Filename, Size, charset(Config)), {file, Filename}}
+                    {200, headers(Filename, Size, charset(Config)),
+                     {file, Filename}}
             end
     end.
 
-handle_event(_, _, _) ->
-    ok.
+handle_event(_, _, _) -> ok.
 
 %%
 %% Config
 %%
 
-default(Config) ->
-    proplists:get_value(default, Config, <<"index.html">>).
+default(Config) -> proplists:get_value(default, Config, <<"index.html">>).
 
-path(Config) ->
-    proplists:get_value(path, Config, <<"/tmp">>).
+path(Config) -> proplists:get_value(path, Config, <<"/tmp">>).
 
-prefix(Config) ->
-    proplists:get_value(prefix, Config, <<>>).
+prefix(Config) -> proplists:get_value(prefix, Config, <<>>).
 
-charset(Config) ->
-    proplists:get_value(charset, Config).
+charset(Config) -> proplists:get_value(charset, Config).
 
 %%
 %% Helpers
@@ -61,67 +56,52 @@ charset(Config) ->
 
 unprefix(RawPath, {regex, Prefix}) ->
     case re:run(RawPath, Prefix, [{capture, all, binary}]) of
-        nomatch ->
-            undefined;
-        _Result ->
-            re:replace(RawPath, Prefix, "", [{return, binary}])
+        nomatch -> undefined;
+        _Result -> re:replace(RawPath, Prefix, "", [{return, binary}])
     end;
 
 unprefix(RawPath, Prefix) ->
     PrefixSz = size(Prefix),
     case RawPath of
-        <<Prefix:PrefixSz/binary, File/binary>> ->
-            File;
-        _ ->
-            undefined
+        <<Prefix:PrefixSz/binary, File/binary>> -> File;
+         _                                      -> undefined
     end.
 
-local_path(Config, <<"/", File/binary>>) ->
-    local_path(Config, File);
+local_path(Config, <<"/", File/binary>>) -> local_path(Config, File);
 
-local_path(Config, <<"">>) ->
-    filename:join(filename:flatten([path(Config), default(Config)]));
+local_path(Config, <<>>) -> join(flatten([path(Config), default(Config)]));
 
 local_path(Config, FilePath) ->
     MappedPath = path(Config),
-    case binary:match(filename:dirname(FilePath), <<"..">>) of
+    case binary:match(dirname(FilePath), <<"..">>) of
         nomatch ->
             case binary:last(FilePath) of
-                $/ ->
-                    filename:join(filename:flatten([MappedPath, FilePath,
-                                                    default(Config)]));
-                _ ->
-                    filename:join(filename:flatten([MappedPath, FilePath]))
+                $/ -> join(flatten([MappedPath, FilePath, default(Config)]));
+                _  -> join(flatten([MappedPath, FilePath]))
             end;
         _       -> undefined
     end.
 
-file_size(undefined) ->
-    {error, illegal_path};
+file_size(undefined) -> {error, illegal_path};
 
 file_size(Filename) ->
     case file:read_file_info(Filename, [{time, posix}]) of
         {ok, #file_info{type = regular, access = Perm, size = Size}}
-                when Perm =:= read orelse Perm =:= read_write ->
+          when Perm =:= read orelse Perm =:= read_write ->
             {ok, Size};
-        {error, Reason} ->
-            {error, Reason};
-        _ ->
-            {error, invalid_file}
+        {error, Reason} -> {error, Reason};
+        _               -> {error, invalid_file}
     end.
 
 headers(Filename, Size, Charset) ->
     case mime_type(Filename) of
-        undefined ->
-            [{"Content-Length", Size}];
-        MimeType ->
-            [{"Content-Length", Size}, {"Content-Type", content_type(MimeType, Charset)}]
+        undefined -> [{"Content-Length", Size}];
+        MimeType  -> [{"Content-Length", Size},
+                      {"Content-Type", content_type(MimeType, Charset)}]
     end.
 
-content_type(MimeType, undefined) ->
-    MimeType;
-content_type(MimeType, Charset) ->
-    MimeType ++ "; charset=" ++ Charset.
+content_type(MimeType, undefined) -> MimeType;
+content_type(MimeType, Charset)   -> MimeType ++ "; charset=" ++ Charset.
 
 %%
 %% Mime types
@@ -129,13 +109,13 @@ content_type(MimeType, Charset) ->
 
 mime_types() ->
     ct_expand:term(
-        dict:from_list(element(2, httpd_conf:load_mime_types(
-                    code:priv_dir(elli_fileserve) ++ "/mime.types")))).
+      dict:from_list(
+        element(2, httpd_conf:load_mime_types(
+                     code:priv_dir(elli_fileserve) ++ "/mime.types")))).
 
 mime_type(Filename) when is_binary(Filename) ->
-    case filename:extension(Filename) of
-        <<>> ->
-            undefined;
+    case extension(Filename) of
+        <<>>               -> undefined;
         <<$., Ext/binary>> ->
             case dict:find(binary_to_list(Ext), mime_types()) of
                 {ok, MimeType} -> MimeType;
